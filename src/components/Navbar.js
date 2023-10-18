338d49515e5973923f62798ad2a621992ff917c7
Kevin <staffordkevinjames@gmail.com>
2023-10-18 16:22:19 -0400

    ADD: Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
 import Searchbar from "./Searchbar";
 import FilterToggle from "./FilterToggle";
+import {Grid, Segment} from 'semantic-ui-react';
 
 function Navbar({handleSearchChange}) {
 
     return (
-        <div class="ui four column grid container">
+        <div class="ui equal width grid">
+          <div class="eight wide column">
+            <div class="ui segment"><Searchbar handleSearchChange={handleSearchChange} />
+            </div>
+        </div>
         <div class="column">
-            <Searchbar handleSearchChange={handleSearchChange} />
-            <FilterToggle />
-            <button>Activities</button>
-            <button>Randomizer</button>
-            <button>Add Activity!</button>
-        </div>
-        </div>
+            <div class="ui segment"><FilterToggle />
+            </div>
+            </div>
+            <div class="column">
+                <div class="ui segment"><button>Activities</button><button>Randomizer</button><button>Add Activity!</button>
+                </div>
+                </div>
+                </div>
     )
 }
 
