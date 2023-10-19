d38044127041cbf70d001b3f3d30abb113c9555e
Kevin <staffordkevinjames@gmail.com>
2023-10-19 18:48:42 -0400

    confetti and db stuff

--- src/components/RandomButton.js
+++ src/components/RandomButton.js
@@ -2,45 +2,47 @@ import React, {useState} from "react"
 import { getRandomActivity } from "./data/randomActivity";
 import RandomActivityCard from "./RandomActivityCard";
 import {Grid} from 'semantic-ui-react';
+import ConfettiExplosion from 'react-confetti-explosion';
 
 
 function RandomButton() {
-    const [randomActivity, setRandomActivity] = useState([])
-    const [isClicked, setIsClicked] = useState(false)
-    
+  const [randomActivity, setRandomActivity] = useState([])
+  const [isClicked, setIsClicked] = useState(false)
+  const [isExploding, setIsExploding] = useState(false)
 
 
-    function handleClick(e) {
-      setRandomActivity(getRandomActivity())
-    }
+  function handleClick(e) {
+    setRandomActivity(getRandomActivity())
+  }
 
-    const toggleActivity = () => {
-      setIsClicked(true)
-    }
+  const toggleActivity = () => {
+    setIsClicked(true)
+    setIsExploding(true)
+  }
     
 
         
         
-    return (
-      <Grid centered>
-        <Grid.Column textAlign="center">
-          <div className="centered">
-            {isClicked ? (
-            <div div className="card-container">
-              <RandomActivityCard randomActivity={randomActivity} />
-            </div>
-              ) : null}
-            </div>
-            <div>
-              <button onClick={handleClick} class="ui basic button" className="buttonStyle">
-                <div onClick={toggleActivity} >Can't Decide?</div>
-                <div onClick={toggleActivity} >Let The AI Choose!</div>
-              </button>
-            </div>
-
-          </Grid.Column>
-        </Grid>
-      );
-    }
+  return (
+    <Grid centered>
+      <Grid.Column textAlign="center">
+        <div className="centered">
+          {isClicked ? (
+          <div div className="card-container">
+            <RandomActivityCard randomActivity={randomActivity} />
+          </div>
+            ) : null}
+          </div>
+          <div>
+            {isExploding ? (<div><ConfettiExplosion /></div>) : null}
+            <button onClick={handleClick} class="ui basic button" className="buttonStyle">
+              <div onClick={toggleActivity} >Can't Decide?</div>
+              <div onClick={toggleActivity} >Let The AI Choose!</div>
+            </button>
+          </div>
+        </Grid.Column>
+      </Grid>
+    );
+  }
   
     export default RandomButton;
